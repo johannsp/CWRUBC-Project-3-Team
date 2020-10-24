b36af2cf86f85d55a5a2cc8332e0ff43ef2e6cbd
John L Spetz <JohnLSpetz@zoho.com>
2020-10-24 15:37:31 -0400

    Hookup for sequelize and mysql2 for database functionality

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
+// Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3001;
+const db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Requiring our routes
+// INSERT THE ROUTE HOOKUP CODE HERE!!!
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -14,6 +23,18 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// Syncing our database and logging a message to the user upon success
+db.sequelize
+  .sync({
+    /* {{{ **
+     * // Drop database and all tables on each restart of server
+     * force: true,
+     * // but only in development account, not test or production
+     * match: /_dev$/
+     * }}} */
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🌎 ==> API server now on port ${PORT}!`);
+    });
+  });
