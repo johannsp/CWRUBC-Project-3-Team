821b31a1ef90de7cc1dab9b97f013c9424af521d
John L Spetz <JohnLSpetz@zoho.com>
2020-10-24 16:00:46 -0400

    Configure sequelize to drop all tables on restart only for classTime_dev

--- server.js
+++ server.js
@@ -26,12 +26,10 @@ app.get("*", function(req, res) {
 // Syncing our database and logging a message to the user upon success
 db.sequelize
   .sync({
-    /* {{{ **
-     * // Drop database and all tables on each restart of server
-     * force: true,
-     * // but only in development account, not test or production
-     * match: /_dev$/
-     * }}} */
+    // Drop database and all tables on each restart of server
+    force: true,
+    // but only in development account, not test or production
+    match: /_dev$/
   })
   .then(() => {
     app.listen(PORT, () => {
