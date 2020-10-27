bb75783d84d655d198657781c885c5be318ba103
John L Spetz <JohnLSpetz@zoho.com>
2020-10-27 18:01:56 -0400

    Incomplete basic hookup of Passport authentication code

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 const path = require("path");
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3001;
@@ -8,6 +11,13 @@ const db = require("./models");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
